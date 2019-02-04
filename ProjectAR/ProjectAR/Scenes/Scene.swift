54610966d4cb351fa534b07c17d0bc698eb2f6b1
Kushal Pandya <pandyak@uoguelph.ca>
2019-02-04 13:23:25 -0500

    initial commit

---
+++ ProjectAR/ProjectAR/Scenes/Scene.swift
@@ -0,0 +1,29 @@
+//
+//  Scene.swift
+//  ProjectAR
+//
+//  Created by Kushal Pandya on 2019-02-04.
+//  Copyright © 2019 Kushal Pandya. All rights reserved.
+//
+
+import SceneKit
+import SpriteKit
+import ARKit
+
+class Scene: SKScene {
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let sceneView = self.view as? ARSKView else {
+            return
+        }
+        
+        if let touchLocation = touches.first?.location(in: sceneView) {
+        
+            // Code to add "model/bird"
+            if let hit = sceneView.hitTest(touchLocation, types: .featurePoint).first {
+                sceneView.session.add(anchor: ARAnchor(transform: hit.worldTransform))
+            }
+        }
+    }
+    
+}
---
+++ ProjectAR/ProjectAR/ViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  ProjectAR
+//
+//  Created by Kushal Pandya on 2019-02-04.
+//  Copyright © 2019 Kushal Pandya. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        // Show statistics such as fps and timing information
+        sceneView.showsStatistics = true
+        
+        // Create a new scene
+        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        
+        // Set the scene to the view
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
+     
+     return node
+     }
+     */
+}
+
+// MARK: - ARSCNViewDelegate
+
+    // TODO: Add ARSCNViewDelegate here
+
+
+// MARK: - ARSKViewDelegate
+
+extension ViewController: ARSKViewDelegate {
+        
+    //    func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+    //
+    //    }
+    
+    func view(_ view: ARSKView, didAdd node: SKNode, for anchor: ARAnchor) {
+        
+    }
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        
+    }
+}
