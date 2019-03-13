caf5bb3acf3154c66dd9ccd4785030472d32fcc0
Michael Truong <mtruon02@mail.uoguelph.ca>
2019-03-13 15:57:06 -0400

    Removed commented code and added comments

--- ProjectAR/ProjectAR/Virtual Objects/VirtualObjectInteraction.swift
+++ ProjectAR/ProjectAR/Virtual Objects/VirtualObjectInteraction.swift
@@ -28,32 +28,12 @@ class VirtualObjectInteraction {
         let planePosition = result.worldTransform.columns.3
         selectedObject!.position = SCNVector3(planePosition.x, planePosition.y, planePosition.z)
         sceneView.scene.rootNode.addChildNode(selectedObject!)
+        
+        // Retrieve the orientation of the plane relative to the world
         let anchoredNode = sceneView.node(for: planeAnchor)
         let anchoredNodeOrientation = anchoredNode!.worldOrientation
-        selectedObject!.eulerAngles.y = .pi * anchoredNodeOrientation.y
         
+        // Rotate the orientation of the selected object to match orientation of the vertical plane
+        selectedObject!.eulerAngles.y = .pi * anchoredNodeOrientation.y
     }
-    
-    //    private func createPainting(with image: UIImage, using result: ARHitTestResult) {
-    //
-    //        // Retrieve frame scene
-    //        let frameScene = SCNScene(named: "models.scnassets/Painting/painting.scn")
-    //        guard let frameNode = frameScene?.rootNode.childNode(withName: "Painting", recursively: false) else {
-    //            return
-    //        }
-    //
-    //        // Retrieve painting node from frame scene hierarchy
-    //        guard let paintingNode = frameScene?.rootNode.childNode(withName: "PaintedImage", recursively: true) else {
-    //            return
-    //        }
-    //        paintingNode.geometry?.firstMaterial?.diffuse.contents = image
-    //
-    //        // Place the frame at the user's touch location
-    //        let planePosition = result.worldTransform.columns.3
-    //        frameNode.position = SCNVector3(planePosition.x, planePosition.y, planePosition.z)
-    //
-    //        // Add frame node to the scene
-    //
-    //        sceneView.scene.rootNode.addChildNode(frameNode)
-    //    }
 }
