a2b5461c7a3999fbf02a05ef14eee0b5949826fc
Michael Truong <mtruon02@mail.uoguelph.ca>
2019-03-23 16:16:37 -0400

    Changed the styling of the alert to be correspond with the human interface guidelines and adjusted the alert message

--- ProjectAR/ProjectAR/ViewController.swift
+++ ProjectAR/ProjectAR/ViewController.swift
@@ -109,13 +109,11 @@ class ViewController: UIViewController {
 //            node.removeFromParentNode()
 //        }
     }
-    @IBAction func resetButtonTouchDown(_ sender: UIButton) {
-//
-    }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
-        let alert = UIAlertController(title: "Restart Scene", message: nil, preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Destructive action"), style: .default, handler: { _ in
+        let resetMessage = "All virtual objects will be removed and the current configuration will be recreated."
+        let alert = UIAlertController(title: "Restart Scene", message: resetMessage, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Destructive action"), style: .destructive, handler: { _ in
             NSLog("The Restart Scene \"OK\" destructive action alert occured.")
             self.restartScene()
             
@@ -126,7 +124,7 @@ class ViewController: UIViewController {
             rewindAnimation.duration = 1.2
             self.resetButton.layer.add(rewindAnimation, forKey: nil)
         }))
-        alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Escape action"), style: .destructive, handler: { _ in
+        alert.addAction(UIAlertAction(title: NSLocalizedString("Cancel", comment: "Escape action"), style: .default, handler: { _ in
             NSLog("The Restart Scene \"Cancel\" escape alert occured.")
         }))
         self.present(alert, animated: true, completion: nil)
